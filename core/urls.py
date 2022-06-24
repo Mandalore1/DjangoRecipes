d0a4a9fc28d5572c50bb3fd620da91e836a22db2
Mandalore <serriy_volk@mail.ru>
2022-06-24 13:48:31 +0500

    Add: base template

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 import core.views
 
 urlpatterns = [
-    path("test/", core.views.test)
+    path("", core.views.home, name="home")
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,8 @@
+from django.contrib import messages
 from django.shortcuts import render
 
 # Create your views here.
 
 
-def test(request):
-    return render(request, "test.html")
+def home(request):
+    return render(request, "home.html")
